514a7dbdb4c98209d67ad2057c3134f6e2a503f5
dominicpam89 <dominicpam89@gmail.com>
2026-04-10 14:09:29 +0700

    update vite.config and tsconfig to repair bug in implement tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,10 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss,
-  ],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname, './src')
-    }
-  }
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
