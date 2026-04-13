0a9f8bbfcfcc1aa2197f0f977441c7410b6c3071
dominicpam89 <dominicpam89@gmail.com>
2026-04-13 12:43:39 +0700

    cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import ThemeToggle from './components/ThemeToggle'
-import { Button } from './components/ui/button'
 export default function App() {
-  return (
-    <div className="bg-amber-800 w-full h-full text-rose-300">
-      Text
-      <Button>Test Button</Button>
-      <ThemeToggle />
-    </div>
-  )
+  return <></>
 }
